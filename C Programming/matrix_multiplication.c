865b2e735f1cfb4702ffc18ae0488b17a7b73c8e
Neup Orng <neuporng18@kit.edu.kh>
2019-03-12 08:08:49 +0000

    matrix multiplication

--- C Programming/matrix_multiplication.c
+++ C Programming/matrix_multiplication.c
@@ -1,90 +1,52 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int matrix[3][3],matrix1[3][3],answer[3][3],i,j,temp; //matrix declared globally instead of locally
 
-void inpMatrix(int flag) //function for inputting matrices; using flag to differentiate between the first and second matrix
+void main()
 {
-	if(flag==0)
+	int  i , j, k, a1, a2, b1, b2;
+	printf("This is the program of matrix multiplication\n");
+	printf("Input the number of row in matrix 1 : ");
+	scanf("%d", &a1);
+	printf("\nInput the number of collum in matrix 1 : ");
+	scanf("%d", &a2);
+	printf("\nInput the number of row in matrix 2 : ");
+	scanf("%d", &b1);
+	printf("\nInput the number of collum in matrix 2 : ");
+	scanf("%d", &b2);
+	int matrix1[a1][a2],matrix2[b1][b2], answer[a1][a2];
+	if (a2 != b1)
 	{
-		printf("Please give me input for the first matrix: \n\n");
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				printf("Matrix[%d][%d]= ",i+1,j+1);
-				scanf("%d",&matrix[i][j]);
-				}	
-			}
-		}	
-	else
-	{
-		printf("\nPlease give me input for the second matrix: \n\n");
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				printf("Matrix[%d][%d]= ",i+1,j+1);
-				scanf("%d",&matrix1[i][j]);
-				}	
-			}
-		}	
-	
-}
-
-void dispMatrix(int flag)
-{
-	if(flag==0)
-	{
-		printf("\nFirst Matrix:\n\n");
-		for (i=0;i<3;i++) {
-	     	for(j=0;j<3;j++) {
-			 	printf("%d ",matrix[i][j]);	
-	     	}
-	     	printf("\n");
-   		}	
+		printf("No answer");
 	}
 	else
 	{
-		printf("\nSecond Matrix:\n\n");
-		for (i=0;i<3;i++) {
-	     	for(j=0;j<3;j++) {
-			 	printf("%d ",matrix1[i][j]);	
-	     	}
-	     	printf("\n");
-   		}
-	}
+		for (i=0;i<a1;i++)
+		{
+			for (j=0;j<a2;j++)
+			{
+				printf("\nEnter the value to the matrix1[%d][%d] : ", i,j);
+				scanf("%d", &matrix1[i][j]);
+			}	
+		}
+		for (i=0;i<b1;i++)
+		{
+			for (j=0;j<b2;j++)
+			{
+			printf("\nEnter the value to the matrix2[%d][%d] : ", i,j);
+			scanf("%d", &matrix2[i][j]);
+			}	
+		}
+		for (i=0;i<a1;i++)
+		{	for(j=0;j<a2;j++)	{
+				answer[i][j]=0;
+				for( k = 0; k< a2; k++){
+					answer[i][j] += matrix1[j][k]*matrix2[k][j];
+				}	}	}
+		printf("answer\n");
+		for (i=0;i<a1;i++)
+		{for(j=0;j<a2;j++){
+				printf("%5d",answer[i][j]);
+		}printf("\n");	}	}
 	
 }
-
-void answerMatrix()
-{
-	printf("\nAnswer:\n\n");
-     	for(temp=0;temp<3;temp++)
-     	{
-      		for (i=0;i<3;i++) {
-       			answer[temp][i] = 0;
-        		for (j=0;j<3;j++){
-
-             		answer[temp][i] = answer[temp][i] + (matrix[temp][j]*matrix1[j][i]);
-        		}
-       		printf("%d ",answer[temp][i]);
-      	}
-      	printf("\n");
-     }
-}
-
-int main() {
-
-	//Inputting  matrix
-   	inpMatrix(0);
-   	inpMatrix(1);
-
-   //printing matrix	
-	dispMatrix(0);
-	dispMatrix(1);
-
-   //calculating matrix multiplication and printing the answer
-   	answerMatrix();
-   	
-    return 0;
-}
+ 
